2488a8673f94af9726e8d3429e6886b9cb2debce
anissarkeraraf <anissarkeraraf@gmail.com>
2024-05-10 21:00:17 +0600

    added the slide

--- src/Componants/Home/Banner/Banner.jsx
+++ src/Componants/Home/Banner/Banner.jsx
@@ -12,9 +12,14 @@ import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import Slide from './Slide';
 
+import bgimg1 from '../../../assets/img1.jpg'
+import bgimg2 from '../../../assets/img2.jpg'
+import bgimg3 from '../../../assets/img3.jpg'
+import bgimg4 from '../../../assets/img4.jpg'
+
 export default function Banner() {
   return (
-    <>
+    <div className='lg:mx-16'>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -31,19 +36,19 @@ export default function Banner() {
         className="mySwiper"
       >
         <SwiperSlide>
-            <Slide></Slide>
+            <Slide image={bgimg1}></Slide>
         </SwiperSlide>
         <SwiperSlide>
-            <Slide></Slide>
+            <Slide image={bgimg2}></Slide>
         </SwiperSlide>
         <SwiperSlide>
-            <Slide></Slide>
+            <Slide image={bgimg3}></Slide>
         </SwiperSlide>
         <SwiperSlide>
-            <Slide></Slide>
+            <Slide image={bgimg4}></Slide>
         </SwiperSlide>
        
       </Swiper>
-    </>
+    </div>
   );
 }
--- src/Componants/Home/Banner/Slide.jsx
+++ src/Componants/Home/Banner/Slide.jsx
@@ -1,16 +1,17 @@
 
 
-const Slide = () => {
+const Slide = ({ image }) => {
     return (
-        <div>
-            <div className="w-full bg-center bg-cover h-[38rem]" style={{
-                backgroundImage: `url('https://i.ibb.co/s9s24nL/young-handsome-physician-medical-robe-with-stethoscope-1303-17818.jpg')`,
+        <div className="mt-16">
+            <div className="w-full bg-center bg-cover h-[300px] md:h-[500px] lg:h-[38rem]" style={{
+                backgroundImage: `url(${image})`,
             }}
             >
                 <div className="flex items-center justify-center w-full h-full bg-gray-900/40">
                     <div className="text-center">
-                        <h1 className="text-3xl font-semibold text-white lg:text-4xl">Build your new <span className="text-blue-400">Saas</span> Project</h1>
-                        <button className="w-full px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
+                        <h1 className="text-2xl md:text-3xl font-semibold text-white lg:text-6xl"><span className="text-[#801818] font-bold">Welcome</span> ! to 7bite <br />Healthcare. How can we help?</h1>
+                        <p className="md:text-xl font-medium text-white mt-4">First communication with our team to enjoying <br /> your new smile with family and friends</p>
+                        <button className=" px-5 py-4 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-[#4245BE] rounded lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">LEARN MORE ABOUT US</button>
                     </div>
                 </div>
             </div>
